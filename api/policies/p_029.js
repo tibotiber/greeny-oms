c305d9a80f63ee7155216de47d70884035854b8b
Thibaut Tiberghien <thibaut.tiberghien@ipal.cnrs.fr>
2015-03-22 12:50:56 +0800

    Generate RBAC policies.

---
+++ api/policies/p_029.js
@@ -0,0 +1,16 @@
+module.exports = function(req, res, next) {
+    if(!(req.session.authenticated && req.session.User))
+	return res.forbidden();
+    var admin			= req.session.User.admin;
+    var manager			= req.session.User.manager;
+    var sales			= req.session.User.sales;
+    var purchasing		= req.session.User.purchasing;
+    var quality_check		= req.session.User.quality_check;
+    var packing			= req.session.User.packing;
+    var documentation		= req.session.User.documentation;
+    var accounts_payable	= req.session.User.accounts_payable;
+    var accounts_receivable	= req.session.User.accounts_receivable;
+    if(admin || packing || quality_check || purchasing || manager)
+	return next();
+    res.forbidden();
+};
