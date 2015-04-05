927a203d245d22c21e58128450d9f4559919d0d4
Thibaut Tiberghien <thibaut.tiberghien@ipal.cnrs.fr>
2015-04-05 23:22:06 +0800

    Clear todomvc tuto related stuff.

--- frontend/app/components/todo-edit.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-import layout from '../templates/components/todo-edit';
-
-export default Ember.TextField.extend({
-    didInsertElement: function() {
-	this.$().focus();
-    }
-});
