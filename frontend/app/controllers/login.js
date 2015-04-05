759c389d20d74c083db3cb4d27527059aa8a1a3f
Thibaut Tiberghien <thibaut.tiberghien@ipal.cnrs.fr>
2015-04-05 16:19:21 +0800

    Better error display on login + replace bootstrap-validator by ember-validations.

--- frontend/app/controllers/login.js
+++ frontend/app/controllers/login.js
@@ -2,5 +2,16 @@ import Ember from 'ember';
 import LoginControllerMixin from 'simple-auth/mixins/login-controller-mixin';
 
 export default Ember.Controller.extend(LoginControllerMixin, {
-    authenticator: 'simple-auth-authenticator:token'
+    actions: {
+	// display an error when authentication fails
+	authenticate: function() {
+	    var _this = this;
+	    var credentials = this.getProperties('identification', 'password');
+	    this.get('session').authenticate('simple-auth-authenticator:token', credentials).then(null, function(error) {
+		var message = error.error;
+		_this.set('errorMessage', message);
+		_this.set('loginAttempts', _this.get('loginAttempts')+1);
+	    });
+	}
+    }
 });
