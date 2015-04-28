363364f5ec5da6c64948fa05dec53d9ac83c210b
Thibaut Tiberghien <thibaut.tiberghien@ipal.cnrs.fr>
2015-04-28 22:53:49 +0800

    Refactor custom form components.

--- frontend/app/components/my-textarea.js
+++ frontend/app/components/my-textarea.js
@@ -1,26 +1,6 @@
-import Ember from 'ember';
+import myInput from './my-input';
 import layout from '../templates/components/my-textarea';
 
-export default Ember.Component.extend({
-
-    layout: layout,
-
-    classNameBindings: ['wrapperClass'],
-
-    shouldValidate: false,
-
-    createBindings: function() {
-	Ember.defineProperty(this, 'val', Ember.computed.alias('parentView.form.'+this.get('id')));
-	Ember.defineProperty(this, 'err', Ember.computed.alias('parentView.form.errors.'+this.get('id')));
-    }.on('init'),
-    
-    error: function() {
-	return (this.get('shouldValidate')) ? this.get('err') : null;
-    }.property('err', 'shouldValidate'),
-
-    actions: {
-	visited: function() {
-	    this.set('shouldValidate', true);
-	}
-    }
+export default myInput.extend({
+    layout: layout
 });
