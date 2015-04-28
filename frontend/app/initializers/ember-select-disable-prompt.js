4423ebc664596ec33968413491a0408885115fb1
Thibaut Tiberghien <thibaut.tiberghien@ipal.cnrs.fr>
2015-04-28 23:25:47 +0800

    Minor fix so jshint doesn't bother me.

--- frontend/app/initializers/ember-select-disable-prompt.js
+++ frontend/app/initializers/ember-select-disable-prompt.js
@@ -3,7 +3,9 @@ import Ember from 'ember';
 export function initialize(/*container, application*/) {
     Ember.Select.reopen({
 	didInsertElement: function() {
-	    if(this.prompt) this.$('option:first').attr('disabled', true);
+	    if(this.prompt) {
+		this.$('option:first').attr('disabled', true);
+	    }
 	}
     });
 }
