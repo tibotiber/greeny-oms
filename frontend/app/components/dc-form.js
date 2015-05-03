ba8205284858d4beb0c31c57381286b2717065e5
Thibaut Tiberghien <thibaut.tiberghien@ipal.cnrs.fr>
2015-05-03 18:27:46 +0800

    Prepare dc-form components

---
+++ frontend/app/components/dc-form.js
@@ -0,0 +1,34 @@
+import Ember from 'ember';
+import layout from '../templates/components/dc-form';
+
+export default Ember.Component.extend({
+
+    layout: layout,
+
+    createBindings: function() {
+	Ember.defineProperty(this, 'isInvalid', Ember.computed.alias('form.isInvalid'));
+	Ember.defineProperty(this, 'errors', Ember.computed.alias('form.errors'));
+    }.on('init'),
+
+    defaults: function() {
+	if(!this.get('buttonLabel')) {
+	    this.set('buttonLabel', 'Save');
+	}
+	if(!this.get('inputWrapperClass')) {
+	    this.set('inputWrapperClass', 'col-xs-12');
+	}
+	if(!this.get('inputClass')) {
+	    this.set('inputClass', 'col-sm-8');
+	}
+	if(!this.get('labelClass')) {
+	    this.set('labelClass', 'col-sm-4');
+	}
+    }.on('init'),
+
+    actions: {
+	post: function() {
+	    this.sendAction('action', this.get('param'));
+	}
+    }
+
+});
---
+++ frontend/app/components/dc-input.js
@@ -0,0 +1,44 @@
+import Ember from 'ember';
+import layout from '../templates/components/dc-input';
+
+export default Ember.Component.extend({
+
+    layout: layout,
+
+    classNameBindings: ['wrapperClass'],
+
+    shouldValidate: false,
+
+    createBindings: function() {
+	Ember.defineProperty(this, 'val', Ember.computed.alias('parentView.form.'+this.get('id')));
+	Ember.defineProperty(this, 'err', Ember.computed.alias('parentView.form.errors.'+this.get('id')));
+    }.on('init'),
+
+    defaults: function() {
+	if(!this.get('label')) {
+	    this.set('label', Ember.String.capitalize(this.get('id')));
+	}
+	if(!this.get('nolabel')) {
+	    this.set('autolabel', true);
+	}
+	if(!this.get('wrapperClass')) {
+	    this.set('wrapperClass', this.get('parentView.inputWrapperClass'));
+	}
+	if(!this.get('inputClass')) {
+	    this.set('inputClass', this.get('parentView.inputClass'));
+	}
+	if(!this.get('labelClass')) {
+	    this.set('labelClass', this.get('parentView.labelClass'));
+	}
+    }.on('init'),
+    
+    error: function() {
+	return (this.get('shouldValidate')) ? this.get('err') : null;
+    }.property('err', 'shouldValidate'),
+
+    actions: {
+	visited: function() {
+	    this.set('shouldValidate', true);
+	}
+    }
+});
