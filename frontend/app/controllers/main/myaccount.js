53536f5297cb191271c2f8417dd36b3c1a4b2e89
Thibaut Tiberghien <thibaut.tiberghien@ipal.cnrs.fr>
2015-05-13 22:40:14 +0800

    Add validation to user profile.

--- frontend/app/controllers/main/myaccount.js
+++ frontend/app/controllers/main/myaccount.js
@@ -1,6 +1,19 @@
 import Ember from 'ember';
+import EmberValidations from 'ember-validations';
 
-export default Ember.ObjectController.extend({
+export default Ember.ObjectController.extend(EmberValidations.Mixin, {
+
+    validations: {
+	name: {
+	    presence: true,
+	    format: { with: /^([a-zA-Z\ ])+$/, allowBlank: true, message: 'must be letters and space only'}
+	},
+	email: {
+	    presence: true,
+	    format: { with: /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/, allowBlank: true, message: 'invalid email address'}
+	}
+    },
+
     actions: {
 	cancel: function() {
 	    this.get('model').rollback();
